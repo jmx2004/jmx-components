555393131d2bc5ada2eb69e790e24d0e4c999835
jmx <123@qq.com>
2025-12-13 22:10:58 +0800

    图标选择器

--- src/components/chooseIcon/src/index.vue
+++ src/components/chooseIcon/src/index.vue
@@ -2,12 +2,29 @@
   <el-button @click="handleClick" type="primary">
     <slot></slot>
   </el-button>
-  <el-dialog :title="title" v-model="visibleProxy">111</el-dialog>
+  <div class="choose-icon-dialog-body-height">
+    <el-dialog :title="title" v-model="visibleProxy">
+      <el-scollbar class="container">
+        <div class="item" v-for="(item, index) in Object.keys(ElIcons)" :key="index"
+        @click="clickItem(item)">
+          <div class="text">
+            <!-- 动态组件 -->
+            <component :is='`el-icon-${toLine(item)}`'></component>
+          </div>
+          <div class="icon">
+            {{ item }}
+          </div>
+        </div>
+      </el-scollbar>
+    </el-dialog>
+  </div>
 </template>
 
 <script lang='ts' setup>
+import * as ElIcons from '@element-plus/icons-vue'
 import { computed } from 'vue'
-
+import { toLine } from '../../../utils'
+import { useCopy } from '../../../hooks/useCopy';
 //  props：使用 modelValue（Vue3 规范）
 const props = defineProps<{
   title: string
@@ -29,9 +46,42 @@ const visibleProxy = computed({
 const handleClick = () => {
   visibleProxy.value = true
 }
+// 点击图标
+let clickItem = (item: String) => {
+  console.log(item);
+  let text = `<el-icon-${toLine(item)} />`
+  // 复制文本
+  useCopy(text)
+  visibleProxy.value = false
 
+}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+}
+.item {
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+  height: 70px;
+}
+.text {
+  font-size: 14px;
+}
+.icon{
+  flex: 1
 
+}
+svg {
+  width: 2em;
+  height: 2em;
+}
 </style>
