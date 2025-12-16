7f66a1a288ba9f29e9b5737804720288f9b33d58
jmx <123@qq.com>
2025-12-16 15:41:18 +0800

    日期选择器

--- src/views/chooseTime/index.vue
+++ src/views/chooseTime/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <x-choose-time  @startChange="startChange" @endChange="endChange"></x-choose-time>
+    <br/>
+    <br/>
+    <x-choose-date @startChange="dateStartChange" @endChange="dateEndChange"></x-choose-date>
 
   </div>
 </template>
@@ -10,10 +13,21 @@ interface endValue {
   startTime: string,
   endTime: string
 }
+interface endDate {
+  startDate: Date,
+  endDate: Date
+
+}
 let startChange = (val: string) => {
   console.log(val);
 }
 let endChange = (val: endValue) => {
+  console.log(val);
+}
+let dateStartChange = (val: Date) => {
+  console.log(val);
+}
+let dateEndChange = (val: endDate) => {
   console.log(val);
 }
 
